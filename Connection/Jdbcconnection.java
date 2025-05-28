82370461257e84d1a1284c9aeeed51f7067aba45
Immu <shaikimam37302@gmail.com>
2025-05-28 10:14:55 +0530

    JDBC Connection

--- Connection/Jdbcconnection.java
+++ Connection/Jdbcconnection.java
@@ -1,12 +1,29 @@
-package Connection;
-//import all the statements which are required for a JDBC connection
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 public class Jdbcconnection {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/JDBC", "root", "Immu@9866");
+       if (con != null) {
+            System.out.println("Connection Established");
+        } else {
+            System.out.println("Connection not Established");
+            return;
+        }
+    }
+}
+
+
+
+//import all the statements which are required for a JDBC connection
+
+// import java.sql.Connection;
+// import java.sql.DriverManager;
+// import java.sql.SQLException;
+
+// public class Jdbcconnection {
+    // public static void main(String[] args) throws SQLException, ClassNotFoundException {
         
         //There is class called Class in java
         //in that we have a method called forName()
@@ -19,7 +36,7 @@ public class Jdbcconnection {
         //step-3: Check weather the connection is sucessfully established or not
 
         // step-1: Load the Driver
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        // Class.forName("com.mysql.cj.jdbc.Driver");
         // A Java static method used to load and register a class by name.
         // When the specified class is loaded, its static blocks are executed automatically.
         // If the driver class is not found, it will throw and print a ClassNotFoundException.
@@ -34,7 +51,7 @@ public class Jdbcconnection {
         //1->database URL
         //2->database username
         //3->database password
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/JDBC", "root", "Immu@9866");
+        // Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/JDBC", "root", "Immu@9866");
         //It returns a Connection object if successful.
 
         //step-3: Check weather the connection is sucessfully established or not
@@ -43,14 +60,14 @@ public class Jdbcconnection {
         // the Connection variable conn will return null
 
         //if conn!=null the connection will be established
-        if (con != null) {
-            System.out.println("Connection Established");
-        } else {
-            System.out.println("Connection not Established");
-            return;
-        }
-    }
-}
+//         if (con != null) {
+//             System.out.println("Connection Established");
+//         } else {
+//             System.out.println("Connection not Established");
+//             return;
+//         }
+//     }
+// }
 
 // Wrapping your code block in a try-except block helps handle connection errors gracefully 
 // rather than letting the program crash when something goes wrong 
